e52fc032df2fc0ec26748fe4f72a3b7c23303ecc
Cesar G <mr.ebola@hotmail.com>
2012-10-17 11:05:51 -0500

    refinery website with custom template

---
+++ app/helpers/application_helper.rb
@@ -0,0 +1,12 @@
+module ApplicationHelper
+	
+	# def find_all_children_pages page
+	# 	children = Refinery::Page.find_all_by_parent_id(page.id)
+	#   if children.empty? && page.parent_id.present?
+	#     children = Refinery::Page.find_all_by_parent_id(page.parent_id)
+	#   end
+
+	#   children
+	# end
+
+end
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Softwareallies::Application.config.secret_token = '56b166ee433e53e7a0e9ebcac4c35bae3adc519d5e92d3d5e2c7c251adad38fb048e2573a63cb34c057b438d8b524902602af4f3a4342d921023206f14aa4648'
